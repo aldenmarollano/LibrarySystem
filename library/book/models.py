f77122fe991106e5ea5223dd3fddbb9ec9d26b37
Alden Marollano <aldenmarollano4b@gmail.com>
2024-07-15 11:46:25 +0800

    Update Templates

--- library/book/models.py
+++ library/book/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
+
 class BookCategory(models.Model):
     category_name = models.CharField(max_length=50)
     category_description = models.TextField(max_length=500)
 
+    def __str__(self):
+        return self.category_name
+
+
 class Author(models.Model):
     auth_fname = models.CharField(max_length=200)
     auth_lname = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.auth_fname} {self.auth_lname}"
+
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
@@ -15,3 +24,5 @@ class Book(models.Model):
     category = models.ForeignKey(BookCategory, on_delete=models.CASCADE, related_name='category')
     author = models.ManyToManyField(Author, related_name='author')
 
+    def __str__(self):
+        return self.title
--- library/borrower/models.py
+++ library/borrower/models.py
@@ -2,15 +2,24 @@ from django.db import models
 from book.models import Book
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+
 class Borrower(models.Model):
     id_num = models.CharField(max_length=1000)
     stud_fname = models.CharField(max_length=200)
     stud_lname = models.CharField(max_length=200)
-    year = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    year = models.IntegerField(validators=[MinValueValidator(1),
+                                           MaxValueValidator(5)])
+ 
+    def __str__(self):
+        return f"{self.stud_fname} {self.stud_lname}"
+
 
 class BorrowedBooks(models.Model):
-    borrower = models.ForeignKey(Borrower, related_name='borrower', on_delete=models.CASCADE)
+    borrower = models.ForeignKey(Borrower, related_name='borrower',
+                                 on_delete=models.CASCADE)
     book = models.ManyToManyField(Book)
-    borrow_date = models.DateTimeField(auto_now=True)
+    borrow_date = models.DateTimeField(auto_now_add=True)
     return_date = models.DateTimeField()
 
+    def __str__(self):
+        return self.book
